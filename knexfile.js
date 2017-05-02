24a3a30d5728c4b69b7cdee33d2432df95cbb216
Hans, Alexander <s66959@beuth-hochschule.de>
2017-05-02 22:54:33 +0200

    switch to heroku clearDB

--- knexfile.js
+++ knexfile.js
@@ -44,7 +44,7 @@ module.exports = {
 		}
 	},
 
-	production: {
+	hosting_agency: {
         client: 'mysql',
         connection: {
             host: 'db.hosting-agency.de',
@@ -56,6 +56,20 @@ module.exports = {
         migrations: {
             tableName: 'knex_migrations'
         }
+	},
+
+	production: {
+		client: 'mysql',
+		connection: {
+			host: 'us-cdbr-iron-east-03.cleardb.net',
+			user: 'bb41eedfd379a8',
+			password: process.env.clearDB_password,
+			database: 'heroku_9b6f95eb7a9adf8',
+			charset: 'utf8'
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
 	}
 
 };
