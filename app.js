87854c44f876a6c88171b46493111cc12b9a5c5e
alexhans1 <alexander.hans.mail@gmail.com>
2017-10-26 20:25:20 +0200

    not load scheduled jobs if local

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/static', express.static('public'));
 
 //use passport & sessions
 app.use(passport.initialize());
@@ -79,9 +78,9 @@ require('./routes/routes.js')(app, passport, Bookshelf);
 //setup scheduled jobs
 if (!((process.env.NODE_ENV === 'local'))) {
 	require('./scheduled_jobs/sendDebtMails');
+	require('./scheduled_jobs/saveTotalClubDebt');
+	require('./scheduled_jobs/checkBankTransactions');
 }
-require('./scheduled_jobs/saveTotalClubDebt');
-require('./scheduled_jobs/checkBankTransactions');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
