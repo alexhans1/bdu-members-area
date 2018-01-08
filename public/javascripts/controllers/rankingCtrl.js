af717087b8ec0c2074571b6e0fbc7af5b03749c0
alexhans1 <alexander.hans.mail@gmail.com>
2018-01-08 16:07:53 +0100

    removed ranking

--- public/javascripts/controllers/rankingCtrl.js
+++ public/javascripts/controllers/rankingCtrl.js
@@ -2,50 +2,8 @@
  * Created by alex.hans on 19.07.2017.
  */
 
-app.controller('RankingCtrl', function ($scope, $rootScope, $http) {
+app.controller('RankingCtrl', function ($scope, $rootScope) {
 	if (!$rootScope.authenticated) {
-		// $location.path('/');
-		$scope.ranking = 'World';
-	} else {
-		//if user is logged in then the user can see the profile page
-		$rootScope.loader = true;
-
-		$http.get('/ranking/all')
-		.then(function(response) {
-			let data = response.data;
-
-			$(function () {
-				let myChart = Highcharts.chart('container', {
-					chart: {
-						type: 'line',
-						zoomType: 'x'
-					},
-					title: {
-						text: 'BDU Ranking'
-					},
-					xAxis: {
-						type: 'datetime'
-					},
-					yAxis: {
-						title: {
-							text: 'Points'
-						},
-						min: -25
-					},
-					tooltip: {
-						shared: false,
-						crosshairs: true
-					},
-					plotOptions: {
-						column: {
-							pointPadding: 0.2,
-							borderWidth: 0
-						}
-					},
-					series: data
-				});
-			});
-			$rootScope.loader = false;
-		});
+		$location.path('/');
 	}
 });
