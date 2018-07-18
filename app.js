eacb6631c6fd67ad1ff2b15ecea6799400471dfd
alexhans1 <mail@alexanderhans.me>
2018-07-18 22:03:28 +0200

    linted frontend

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-let sslRedirect = require('heroku-ssl-redirect');
-let express = require('express');
-let app = express();
-let path = require('path');
-if (process.env.NODE_ENV !== 'production'){ //only if not production
-	require('longjohn'); // for extensive logging in local
+const sslRedirect = require('heroku-ssl-redirect');
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') { // only if not production
+  require('longjohn'); // for extensive logging in local
 }
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-let dotenv = require('dotenv'); //enables environment variables for development
-dotenv.load();
-console.log(process.env.NODE_ENV);
-require('console-stamp')( console, { pattern : "dd/mm/yyyy HH:MM:ss" } ); //adds a timestamp to each console log
-let flash = require('req-flash'); //lets me parse individual messages to requests
-let session = require('express-session'); //browser sessions for authentication
-let passport = require('passport'); //Passport is the library we will use to handle storing users within HTTP sessions
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv'); // enables environment variables for development
+if (process.env.NODE_ENV !== 'production') dotenv.load();
+require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss' }); // adds a timestamp to each console log
+const flash = require('req-flash'); // lets me parse individual messages to requests
+const session = require('express-session'); // browser sessions for authentication
+const passport = require('passport'); // Passport is the library we will use to handle storing users within HTTP sessions
 
 // connect to database
-let conn, knex, Bookshelf;
+let conn,
+  knex,
+  Bookshelf;
 try {
-	conn = require('./knexfile.js'); //read out the DB Conn Data
-	knex = require('knex')(conn[process.env.NODE_ENV || 'local']); //require knex query binder
-	Bookshelf = require('bookshelf')(knex); //require Bookshelf ORM Framework
+  conn = require('./knexfile.js'); // read out the DB Conn Data
+  knex = require('knex')(conn[process.env.NODE_ENV || 'local']); // require knex query binder
+  Bookshelf = require('bookshelf')(knex); // require Bookshelf ORM Framework
 } catch (ex) {
-	console.log(ex);
+  console.log(ex);
 }
 
-//IF NEW ROUTE IS CREATED, DEFINE NEW ROUTE HERE AND SET URI FURTHER DOWN
-//sets up routes variables
-let index = require('./routes/index');
-let restApi = require('./routes/restAPI')(Bookshelf);
-let rankingApi = require('./routes/rankingAPI')(Bookshelf);
-let dashboard = require('./routes/dashboardAPI')(Bookshelf);
+// IF NEW ROUTE IS CREATED, DEFINE NEW ROUTE HERE AND SET URI FURTHER DOWN
+// sets up routes variables
+const index = require('./routes/index');
+const restApi = require('./routes/restAPI')(Bookshelf);
+const rankingApi = require('./routes/rankingAPI')(Bookshelf);
+const dashboard = require('./routes/dashboardAPI')(Bookshelf);
 
 // https redirect
 app.use(sslRedirect());
@@ -41,17 +44,17 @@ app.use(sslRedirect());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//---------Middleware--------------
+// ---------Middleware--------------
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'BDU.png')));
 app.use(logger('dev'));
 
-//initialize sessions middleware, set secret to 'keyboard cat' to be used by cookies later I guess ?
+// initialize sessions middleware, set secret to 'keyboard cat' to be used by cookies later I guess ?
 app.use(session({
-	secret: 'keyboard cat2',
-	resave: true,
-	saveUninitialized: true
+  secret: 'keyboard cat2',
+  resave: true,
+  saveUninitialized: true,
 }));
 
 app.use(flash());
@@ -60,44 +63,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//use passport & sessions
+// use passport & sessions
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Initialize Passport using passport-init.js
-let initPassport = require('./config/passport-init');
+const initPassport = require('./config/passport-init');
+
 initPassport(passport, Bookshelf);
 
-//setup routes URIs
+// setup routes URIs
 app.use('/', index);
 app.use('/app', restApi);
 app.use('/ranking', rankingApi);
 app.use('/dashboard', dashboard);
 require('./routes/routes.js')(app, passport, Bookshelf);
 
-//setup scheduled jobs
+// setup scheduled jobs
 if (process.env.NODE_ENV === 'production') {
-	require('./scheduled_jobs/sendDebtMails');
-	require('./scheduled_jobs/saveTotalClubDebt');
-	require('./scheduled_jobs/checkBankTransactions');
+  require('./scheduled_jobs/sendDebtMails');
+  require('./scheduled_jobs/saveTotalClubDebt');
+  require('./scheduled_jobs/checkTransactionsService/checkBankTransactions');
 }
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	let err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
