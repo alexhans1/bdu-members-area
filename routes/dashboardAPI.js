7f365ba0acd8d244c10451ca689c5df313f41013
alexhans1 <mail@alexanderhans.me>
2018-07-22 13:55:34 +0200

    refactored app.js and routes

--- routes/dashboardAPI.js
+++ routes/dashboardAPI.js
@@ -1,71 +1,40 @@
-/**
- * Created by alex.hans on 29.07.2017.
- */
+const moment = require('moment');
 
-let express = require('express');
-let router = express.Router();
-let _ = require('lodash');
-let moment = require('moment');
+module.exports = ({ router, Bookshelf, isAuthenticated }) => {
+  console.info('> adding dashboard routes...');
+  // Register the authentication middleware
+  // for all URIs use the isAuthenticated function
+  router.use('/clubdebt', isAuthenticated);
 
-//Used for routes that must be authenticated.
-function isAuthenticated (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
+  const Models = require('../models/bookshelfModels.js')(Bookshelf);
 
-	if (req.isAuthenticated()){
-		return next();
-	}
+  console.info('> > adding get clubDebt route...');
+  router.route('/clubDebt')
+  // fetch all users
+    .get((req, res) => {
+      const result = {
+        name: 'Club Debt',
+        data: [],
+      };
 
-	// if the user is not authenticated then redirect him to the login page
-	console.log('not logged in');
-	return res.redirect('/');
-}
+      try {
+        Models.Club_Debt_Col.forge().fetch()
+          .then((clubDebtCollection) => {
+            console.info('Successfully retrieved all daily club debt.');
+            clubDebtCollection = clubDebtCollection.toJSON();
 
-module.exports = function(Bookshelf){
+            clubDebtCollection.forEach((obj) => {
+              result.data.push([
+                moment(obj.timestamp).unix() * 1000,
+                obj.debt,
+              ]);
+            });
+            res.send(result);
+          });
+      } catch (ex) {
+        console.log(ex);
+      }
+    });
 
-	//Register the authentication middleware
-	//for all URIs use the isAuthenticated function
-	router.use('/', isAuthenticated);
-
-	// --------------------------------------------------------------------------
-	// ------------------------------MODELS--------------------------------------
-	// --------------------------------------------------------------------------
-
-	let Models = require('../models/bookshelfModels.js')(Bookshelf);
-
-	// --------------------------------------------------------------------------
-	// ------------------------------USER REST API-------------------------------
-	// --------------------------------------------------------------------------
-
-	router.route('/clubDebt')
-	// fetch all users
-	.get(function (req, res) {
-
-		let result = {
-			name: 'Club Debt',
-			data: []
-		};
-
-		try {
-			Models.Club_Debt_Col.forge().fetch()
-			.then((clubDebtCollection) => {
-				console.info('Successfully retrieved all daily club debt.');
-				clubDebtCollection = clubDebtCollection.toJSON();
-
-				clubDebtCollection.forEach((obj) => {
-					result.data.push([
-						moment(obj.timestamp).unix()*1000,
-						obj.debt
-					])
-				});
-				res.send(result);
-			});
-		} catch (ex) {
-		    console.log(ex);
-		}
-
-	});
-
-	return router;
+  return router;
 };
