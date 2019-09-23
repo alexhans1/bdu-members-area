9baaa930dc67ee173904c60cff0e5ff22518bab2
alexhans <mail@alexanderhans.me>
2019-09-23 14:06:30 +0200

    cleaned up navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,142 +1,221 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from 'react';
+import React from 'react';
 import './Navbar.scss';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  User,
+  List,
+  LogOut,
+  Award,
+  LogIn,
+  UserPlus,
+  Shield,
+  Calendar,
+  Users,
+  Edit,
+} from 'react-feather';
 import { logout } from '../../actions/AuthActions';
 import logo from './bdu_white_logo.png';
+import { calculateTotalUserDebt } from '../../helpers';
 
-const mapStateToProps = ({ user }) => ({
-  isAuthenticated: user.isAuthenticated,
-  authenticatedUser: user.authenticatedUserId
-    ? user.users.find(({ id }) => user.authenticatedUserId === id)
-    : {},
-  isAdmin: user.authenticatedUserId
-    ? user.users.find(({ id }) => user.authenticatedUserId === id).position ===
-      1
-    : false,
-});
+const NavbarDropdown = ({ link, pathname = '/', children = [] }) => {
+  const childrenContainActiveLink = children
+    .map(child => `/${child.props.link.url}` || '')
+    .includes(pathname);
 
-const mapDispatchToProps = { logout };
+  return (
+    <div
+      className={`nav-item ${
+        childrenContainActiveLink ? 'active' : ''
+      } dropdown ${link.className}`}
+    >
+      <a
+        className="nav-link dropdown-toggle"
+        href="#"
+        id="navbarDropdown"
+        role="button"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        {link.icon}
+        {link.title}
+      </a>
+      <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+        {children}
+      </div>
+    </div>
+  );
+};
 
-class Navbar extends Component {
-  render() {
-    const {
-      isAuthenticated,
-      authenticatedUser,
-      isAdmin,
-      logout: handleLogout,
-    } = this.props;
-
-    const navbarLinks = !isAuthenticated ? (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
-            <i className="fas fa-sign-in-alt" /> Login
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/signup">
-            <i className="fas fa-user-circle" /> Signup
-          </Link>
-        </li>
-      </ul>
-    ) : (
-      <ul className="navbar-nav d-flex w-100">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Tournaments
-          </Link>
-        </li>
-        {isAdmin ? (
-          <li className="nav-item dropdown cursorPointer">
-            <a
-              className="nav-link dropdown-toggle"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Admin
-            </a>
-            <div
-              className="dropdown-menu bg-dark"
-              aria-labelledby="navbarDropdown"
-            >
-              <Link to="/createTournament" className="dropdown-item text-white">
-                Create Tournament
-              </Link>
-              <Link to="/member" className="dropdown-item text-white">
-                Members
-              </Link>
-            </div>
-          </li>
-        ) : null}
-        <li
-          id="profileDropdown"
-          className="nav-item dropdown cursorPointer ml-auto"
-        >
-          <a
-            className="nav-link dropdown-toggle"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <span>{authenticatedUser.vorname}</span>
-          </a>
-          <div
-            className="dropdown-menu dropdown-menu-right bg-dark"
-            aria-labelledby="navbarDropdown"
-          >
-            <Link to="/home" className="dropdown-item text-white">
-              My Tournaments
-            </Link>
-            <Link to="/edit" className="dropdown-item text-white">
-              Edit Profile
-            </Link>
-            <div className="dropdown-divider border-secondary" />
-            <a
-              className="dropdown-item text-white"
-              role="button"
-              tabIndex="0"
-              onClick={handleLogout}
-            >
-              <i className="fas fa-sign-out-alt" /> Logout
-            </a>
-          </div>
-        </li>
-      </ul>
-    );
-
-    return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
-          <img src={logo} width="80" height="35" alt="logo" />
-        </Link>
+const NavbarLink = ({ link, pathname = '/' }) => {
+  const isLinkActive = `/${link.url || ''}` === pathname;
+  return (
+    <li className={`nav-item ${isLinkActive ? 'active' : ''}`}>
+      {link.onClick ? (
         <button
-          className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className={`nav-link d-flex align-items-center btn btn-link ${link.className}`}
+          onClick={link.onClick}
         >
-          <span className="navbar-toggler-icon" />
+          {link.icon}
+          {link.title}
         </button>
+      ) : (
+        <Link
+          className={`nav-link d-flex align-items-center ${link.className}`}
+          key={link.title}
+          to={`/${link.url ? link.url : ''}`}
+        >
+          {link.icon}
+          {link.title}
+        </Link>
+      )}
+    </li>
+  );
+};
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {navbarLinks}
-        </div>
-      </nav>
+const Navbar = ({
+  history: {
+    location: { pathname },
+  },
+}) => {
+  const dispatch = useDispatch();
+  const { isAuthenticated, authenticatedUser, isAdmin } = useSelector(
+    ({ user }) => ({
+      isAuthenticated: user.isAuthenticated,
+      authenticatedUser: user.authenticatedUserId
+        ? user.users.find(({ id }) => user.authenticatedUserId === id)
+        : {},
+      isAdmin: user.authenticatedUserId
+        ? user.users.find(({ id }) => user.authenticatedUserId === id)
+            .position === 1
+        : false,
+    }),
+  );
+
+  const navbarLinks = [];
+  if (isAuthenticated) {
+    navbarLinks.push({
+      title: 'Tournaments',
+      icon: <Award size={18} className="mr-1" />,
+    });
+  } else {
+    navbarLinks.push(
+      ...[
+        {
+          title: 'Login',
+          url: 'login',
+          icon: <LogIn size={18} className="mr-1" />,
+        },
+        {
+          title: 'Signup',
+          url: 'signup',
+          icon: <UserPlus size={18} className="mr-1" />,
+        },
+      ],
     );
   }
-}
+  if (isAdmin) {
+    navbarLinks.push({
+      title: 'Admin',
+      icon: <Shield size={18} className="mr-1" />,
+      children: [
+        {
+          title: 'Create Tournament',
+          url: 'createTournament',
+          icon: <Calendar size={18} className="mr-1" />,
+        },
+        {
+          title: 'Members',
+          url: 'member',
+          icon: <Users size={18} className="mr-1" />,
+        },
+      ],
+    });
+  }
+  if (isAuthenticated) {
+    const totalDebtOfUser = calculateTotalUserDebt(authenticatedUser);
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Navbar);
+    navbarLinks.push({
+      title: (
+        <span
+          className={`badge badge-${totalDebtOfUser > 0 ? 'danger' : 'info'}`}
+        >
+          {Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR',
+          }).format(totalDebtOfUser)}
+        </span>
+      ),
+      icon: <User size={18} className="mr-1" />,
+      className: 'ml-auto align-collapse-right',
+      children: [
+        {
+          title: 'My Tournaments',
+          url: 'home',
+          icon: <List size={18} className="mr-1" />,
+          className: 'ml-auto',
+        },
+        {
+          title: 'Edit Profile',
+          url: 'edit',
+          icon: <Edit size={18} className="mr-1" />,
+        },
+        {
+          title: 'Logout',
+          onClick: () => {
+            dispatch(logout());
+          },
+          icon: <LogOut size={18} className="mr-1" />,
+        },
+      ],
+    });
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        <img src={logo} className="mr-1" width="80" alt="logo" />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav w-100">
+          {navbarLinks.map(link =>
+            link.children ? (
+              <NavbarDropdown link={link} pathname={pathname} key={link.title}>
+                {link.children.map(_link => (
+                  <NavbarLink
+                    link={_link}
+                    pathname={pathname}
+                    key={`${_link.url}_${_link.title}`}
+                  />
+                ))}
+              </NavbarDropdown>
+            ) : (
+              <NavbarLink
+                link={link}
+                key={`${link.url}_${link.title}`}
+                pathname={pathname}
+              />
+            ),
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
