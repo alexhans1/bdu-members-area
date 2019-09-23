00f9d84d6c89bcd585ba819d8ea37391ba6a92cf
alexhans <mail@alexanderhans.me>
2019-09-23 14:08:58 +0200

    made the year in the footer dynamic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,10 @@ const Footer = () => {
       id="footer"
       className="d-flex justify-content-around align-items-center py-2"
     >
-      <span>© Berlin Debating Union e.V., {new Date().getFullYear()}</span>
+      <span>
+© Berlin Debating Union e.V.,
+        {new Date().getFullYear()}
+      </span>
       <Link to="/bug">Feedback / Bugs</Link>
     </footer>
   );
