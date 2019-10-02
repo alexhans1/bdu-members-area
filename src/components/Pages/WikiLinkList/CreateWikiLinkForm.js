9ad7ca173bc57a1d06b6f0a6cf09a39390e13f96
alexhans <mail@alexanderhans.me>
2019-10-02 12:27:36 +0200

    styled form

--- src/components/Pages/WikiLinkList/CreateWikiLinkForm.js
+++ src/components/Pages/WikiLinkList/CreateWikiLinkForm.js
@@ -30,56 +30,52 @@ const CreateWikiLinkForm = () => {
   };
 
   return (
-    <div className="col-12 col-md-8 col-lg-7 col-xl-5 px-0 pb-4">
+    <div className="col-12 px-0 pb-4">
       <h2>Add Link to Document</h2>
-      <form onSubmit={postWikiLink}>
-        <div className="row">
-          <div className="col-12 col-md-10 d-flex flex-wrap">
-            <div className="form-group px-lg-3">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                className="form-control"
-                name="title"
-                autoComplete="title"
-                placeholder="Title"
-                required
-              />
-            </div>
-            <div className="form-group px-lg-3">
-              <label htmlFor="topic">Topic</label>
-              <input
-                type="text"
-                className="form-control"
-                name="topic"
-                autoComplete="topic"
-                placeholder="Topic"
-                required
-              />
-            </div>
-            <div className="form-group px-lg-3">
-              <label htmlFor="url">Link</label>
-              <input
-                type="url"
-                className="form-control"
-                name="url"
-                autoComplete="url"
-                placeholder="The Link"
-                required
-              />
-            </div>
-            <div className="form-group px-lg-3">
-              <label htmlFor="description">Description</label>
-              <textarea
-                rows="5"
-                className="form-control"
-                name="description"
-                autoComplete="description"
-                placeholder="Description"
-                required
-              />
-            </div>
-          </div>
+      <form className="wiki-links-form" onSubmit={postWikiLink}>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            autoComplete="title"
+            placeholder="Title"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="topic">Topic</label>
+          <input
+            type="text"
+            className="form-control"
+            name="topic"
+            autoComplete="topic"
+            placeholder="Topic"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="url">Link</label>
+          <input
+            type="url"
+            className="form-control"
+            name="url"
+            autoComplete="url"
+            placeholder="The Link"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            rows="4"
+            className="form-control"
+            name="description"
+            autoComplete="description"
+            placeholder="Description"
+            required
+          />
         </div>
         <button type="submit" className="btn btn-outline-info">
           Add Link
