97cd75981057f79ed215afefc7a91e439c9e8e55
alexhans <mail@alexanderhans.me>
2019-12-08 18:43:59 +0100

    changed db connection

--- jobs/banking/index.js
+++ jobs/banking/index.js
@@ -18,9 +18,9 @@ exports.logger = logger;
 const dbConnection = {
   client: 'mysql',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: '+asd',
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    user: 'bb41eedfd379a8',
+    password: process.env.clearDB_password,
     database: 'heroku_9b6f95eb7a9adf8',
     charset: 'utf8',
   },
